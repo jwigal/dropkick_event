694f7f8d3137912796b3e5b3d70f08d0d485954e
Jeff Wigal <jeff@assignr.com>
2012-01-23 08:40:30 -0500

    refactor tests an fix up descriptions

--- test/test_dropkick_event.rb
+++ test/test_dropkick_event.rb
@@ -2,27 +2,21 @@ require 'helper'
 
 class TestDropkickEvent < Test::Unit::TestCase
     
-  should "call the listener" do
-    PasswordResetListener.any_instance.expects(:password_reset).once
-    u=User.new("Joe", "Smith")
-    UserEvent.new.reset(u)
-  end
-  
   
   context "an event" do
     setup do
       @ue = UserEvent.new
     end
 
-    should "any class-defined listeners" do
+    should "ensure that any class-defined listeners are included in the instance method .listeners" do
       assert @ue.listeners.any?{|x| x.class == PasswordResetListener }
     end
     
-    should "ensure that events with class events" do
+    should "ensure that class defined listeners are instances of actual events" do
       assert UserEvent.class_listeners.all?{|l| l.class != Class }
     end
 
-    should "enumerate any per-event listeners" do   
+    should "ensure that any instance-defined listeners are included in the instance method .listeners" do   
       @ue.add_listener AuditListener.new
       assert @ue.listeners.any?{|x| x.class == AuditListener }
     end
